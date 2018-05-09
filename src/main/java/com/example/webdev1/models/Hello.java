3a94879edde6e3c2f5a44f7c0b5c9bad628437ec
parshva45 <parshva45@gmail.com>
2018-05-08 23:33:29 -0400

    fix

--- src/main/java/com/example/webdev1/models/Hello.java
+++ src/main/java/com/example/webdev1/models/Hello.java
@@ -1,9 +1,11 @@
 package com.example.webdev1.models;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Hello {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
--- src/test/java/com/example/webdev1/DemoApplicationTests.java
+++ src/test/java/com/example/webdev1/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.webdevsummer12018;
+package com.example.webdev1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;

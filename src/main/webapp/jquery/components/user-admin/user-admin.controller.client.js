145da6365ea7c32dfe7a70bccff00e2d1b329cf6
parshva45 <parshva45@gmail.com>
2018-05-13 18:36:09 -0400

    Update user details by user admin

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -6,11 +6,26 @@
     var tbody;
     var template;
     var userService = new UserServiceClient()
+    var updateId;
+    var $usernameFld;
+    var $password;
+    var $firstNameFld;
+    var $lastNameFld;
+    var $roleFld;
+    var $updateBtn;
 
     function main() {
         tbody = $('tbody');
-        template = $('.template');
+        template = $('.wbdv-template');
         $('#createUser').click(createUser);
+        //$('.wbdv-update').click(updateUser);
+        $usernameFld = $('#usernameFld');
+        $password = $('#passwordFld');
+        $firstNameFld = $('#firstNameFld');
+        $lastNameFld = $('#lastNameFld');
+        $roleFld = $('#roleFld');
+        $updateBtn = $("#updateBtn")
+            .click(updateUser);
 
         findAllUsers();
     }
@@ -22,11 +37,11 @@
     }
 
     function createUser() {
-        var username = $('#usernameFld').val();
-        var password = $('#passwordFld').val();
-        var firstName = $('#firstNameFld').val();
-        var lastName = $('#lastNameFld').val();
-        var role = $('#roleFld').val();
+        var username = $usernameFld.val();
+        var password = $password.val();
+        var firstName = $firstNameFld.val();
+        var lastName = $lastNameFld.val();
+        var role = $roleFld.val();
 
         var user = {
             username: username,
@@ -49,16 +64,16 @@
 
             clone.attr('id', user.id);
 
-            clone.find('.delete').click(deleteUser);
-            clone.find('.edit').click(editUser);
+            clone.find('.wbdv-delete').click(deleteUser);
+            clone.find('.wbdv-edit').click(editUser);
 
-            clone.find('.username')
+            clone.find('.wbdv-username')
                 .html(user.username);
-            clone.find('.firstName')
+            clone.find('.wbdv-firstName')
                 .html(user.firstName);
-            clone.find('.lastName')
+            clone.find('.wbdv-lastName')
                 .html(user.lastName);
-            clone.find('.role')
+            clone.find('.wbdv-role')
                 .html(user.role);
             tbody.append(clone);
         }
@@ -77,8 +92,43 @@
     }
 
     function editUser(event) {
-        console.log('editUser');
-        console.log(event);
+        var editBtn = $(event.currentTarget);
+        var $tr = editBtn
+            .parent()
+            .parent();
+        updateId = $tr.attr('id');
+        var username = $tr.find('.wbdv-username').text();
+        $usernameFld.val(username);
+        var firstName = $tr.find('.wbdv-firstName').text();
+        $firstNameFld.val(firstName);
+        var lastName = $tr.find('.wbdv-lastName').text();
+        $lastNameFld.val(lastName);
+        var role = $tr.find('.wbdv-role').text();
+        $roleFld.val(role);
+    }
+
+    function updateUser(event) {
+        var user = {
+            firstName: $firstNameFld.val(),
+            lastName: $lastNameFld.val(),
+            role: $roleFld.val()
+        };
+
+        userService
+            .updateUser(updateId, user)
+            .then(success);
+    }
+
+    function success(response) {
+        if(response === null) {
+            alert('unable to update')
+        } else {
+            findAllUsers();
+            $usernameFld.val('');
+            $firstNameFld.val('');
+            $lastNameFld.val('');
+            $roleFld.val('Student');
+        }
     }
 
 })();

94174f6ea1c8fac158a6c4bfd9908d785500b9fe
parshva45 <parshva45@gmail.com>
2018-05-14 21:35:28 -0400

    updating user.sql

--- src/main/java/com/example/webdev1/models/user.sql
+++ src/main/java/com/example/webdev1/models/user.sql
@@ -1,9 +1,12 @@
-select * from userCREATE TABLE `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `role` varchar(255) DEFAULT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=143 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=293 DEFAULT CHARSET=utf8;

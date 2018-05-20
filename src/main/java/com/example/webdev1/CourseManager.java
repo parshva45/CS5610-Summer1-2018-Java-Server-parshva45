46b60ca45294b24123839fb48b00dfab11268f47
parshva45 <parshva45@gmail.com>
2018-05-19 20:39:52 -0400

    adding CourseManager

---
+++ src/main/java/com/example/webdev1/CourseManager.java
@@ -0,0 +1,12 @@
+package com.example.webdev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManager.class, args);
+	}
+}

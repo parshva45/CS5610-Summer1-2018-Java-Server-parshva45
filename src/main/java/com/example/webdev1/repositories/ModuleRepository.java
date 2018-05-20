bf75e753a778e26ea5cbab2e209a5c9762fbf72e
parshva45 <parshva45@gmail.com>
2018-05-19 20:48:38 -0400

    adding Course and Module repositories

---
+++ src/main/java/com/example/webdev1/repositories/ModuleRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdev1.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdev1.models.Module;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer>{
+
+}

c82ab456db20a794056803a577b9ce49b644934b
parshva45 <parshva45@gmail.com>
2018-06-03 19:55:57 -0400

    updating Widget model

--- src/main/java/com/example/webdev1/models/Widget.java
+++ src/main/java/com/example/webdev1/models/Widget.java
@@ -4,90 +4,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 public class Widget {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	private String name;
-	private int position;
 	private String text;
-	private String className;
-	private String style;
-	private String width;
-	private String height;
 	private String widgetType;
-	private int size;
-	private String href;
-	private String src;
-	private String listItems;
-	private String listType;
 	@ManyToOne
 	@JsonIgnore
 	private Lesson lesson;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPosition() {
-		return position;
-	}
-	public void setPosition(int position) {
-		this.position = position;
-	}
 	public String getWidgetType() {
 		return widgetType;
 	}
 	public void setWidgetType(String widgetType) {
 		this.widgetType = widgetType;
 	}
-	public int getSize() {
-		return size;
+	public int getId() {
+		return id;
 	}
-	public void setSize(int size) {
-		this.size = size;
-	}
-	public String getHref() {
-		return href;
-	}
-	public void setHref(String href) {
-		this.href = href;
-	}
-	public String getSrc() {
-		return src;
-	}
-	public void setSrc(String src) {
-		this.src = src;
-	}
-	public String getListItems() {
-		return listItems;
-	}
-	public void setListItems(String listItems) {
-		this.listItems = listItems;
-	}
-	public String getListType() {
-		return listType;
-	}
-	public void setListType(String listType) {
-		this.listType = listType;
-	}
-	public Lesson getLesson() {
-		return lesson;
-	}
-	public void setLesson(Lesson lesson) {
-		this.lesson = lesson;
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getText() {
 		return text;
@@ -95,28 +39,10 @@ public class Widget {
 	public void setText(String text) {
 		this.text = text;
 	}
-	public String getClassName() {
-		return className;
+	public Lesson getLesson() {
+		return lesson;
 	}
-	public void setClassName(String className) {
-		this.className = className;
-	}
-	public String getStyle() {
-		return style;
-	}
-	public void setStyle(String style) {
-		this.style = style;
-	}
-	public String getWidth() {
-		return width;
-	}
-	public void setWidth(String width) {
-		this.width = width;
-	}
-	public String getHeight() {
-		return height;
-	}
-	public void setHeight(String height) {
-		this.height = height;
+	public void setLesson(Lesson lesson) {
+		this.lesson = lesson;
 	}
 }

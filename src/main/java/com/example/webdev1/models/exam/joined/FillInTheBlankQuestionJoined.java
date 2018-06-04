180ac3e56bcdfa986d95c7c8771cf62b7438bfda
parshva45 <parshva45@gmail.com>
2018-06-04 11:12:26 -0400

    adding FillInTheBlankQuestionJoined model

---
+++ src/main/java/com/example/webdev1/models/exam/joined/FillInTheBlankQuestionJoined.java
@@ -0,0 +1,19 @@
+package com.example.webdev1.models.exam.joined;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JOINED_FILL_IN_THE_BLANK_QUESTION")
+public class FillInTheBlankQuestionJoined
+	extends BaseQuestionJoined {
+	@Column(name = "VARIABLES", nullable = false)
+	private String variables;
+	public String getVariables() {
+		return variables;
+	}
+	public void setVariables(String variables) {
+		this.variables = variables;
+	}
+}

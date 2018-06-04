c80a8d5b04c16616dfd654bf881715182305c455
parshva45 <parshva45@gmail.com>
2018-06-04 11:13:29 -0400

    adding FillInTheBlankQuestionRepositoryJoined repository

---
+++ src/main/java/com/example/webdev1/repositories/exam/joined/FillInTheBlankQuestionRepositoryJoined.java
@@ -0,0 +1,10 @@
+package com.example.webdev1.repositories.exam.joined;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdev1.models.exam.joined.FillInTheBlankQuestionJoined;
+
+public interface FillInTheBlankQuestionRepositoryJoined
+	extends CrudRepository<FillInTheBlankQuestionJoined, Integer>{
+
+}
